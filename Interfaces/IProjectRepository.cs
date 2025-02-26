f340bfda05f3db27af3dd30134b108930ae356de
Alec Christensen <alec_christensen@hotmail.com>
2025-02-26 22:38:23 +0100

    Added Project Repository

---
+++ Interfaces/IProjectRepository.cs
@@ -0,0 +1,12 @@
+﻿using ProjectManagmentConsoleApp.Entities;
+
+namespace ProjectManagmentConsoleApp.Interfaces;
+
+public interface IProjectRepository
+{
+    void AddProject(Project project);
+    Project? GetProjectById(int id);
+    List<Project> GetAllProjects();
+    void UpdateProject(Project project);
+    void DeleteProject(int id);
+}
---
+++ Repositories/ProjectRepository.cs
@@ -0,0 +1,47 @@
+﻿using ProjectManagmentConsoleApp.Data;
+using ProjectManagmentConsoleApp.Entities;
+using ProjectManagmentConsoleApp.Interfaces;
+
+namespace ProjectManagmentConsoleApp.Repositories;
+
+public class ProjectRepository : IProjectRepository
+{
+    private readonly AppDbContext _context;
+
+    public ProjectRepository(AppDbContext context)
+    {
+        _context = context;
+    }
+
+    public void AddProject(Project project)
+    {
+        _context.Projects.Add(project);
+        _context.SaveChanges();
+    }
+
+    public Project? GetProjectById(int id)
+    {
+        return _context.Projects.Find(id);
+    }
+
+    public List<Project> GetAllProjects()
+    {
+        return _context.Projects.ToList();
+    }
+
+    public void UpdateProject(Project project)
+    {
+        _context.Projects.Update(project);
+        _context.SaveChanges();
+    }
+
+    public void DeleteProject(int id)
+    {
+        var project = _context.Projects.Find(id);
+        if (project != null)
+        {
+            _context.Projects.Remove(project);
+            _context.SaveChanges();
+        }
+    }
+}
